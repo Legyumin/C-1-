b6d07c2ea309788683208b4a3271532bc749e494
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-08-21 18:01:52 +0900

    인터페이스와 추상클래스

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+
+	abstract class AbstractBase
+	{
+		public void PrivateMethodA()
+		{
+			Console.WriteLine("AbstractBase.PrivateMethodA()");
+		}
+		public void PublicMethodA()
+		{
+			Console.WriteLine("AbstractBase.PublicMethodA()");
+		}
+
+		public abstract void AbstractMethodA();
+	}
+		class Derived : AbstractBase
+		{/*
+		public void PrivateMethodA() {
+			Console.WriteLine("PrivateMethodA()");
+		}*/
+			public override void AbstractMethodA() { //override,abstract 같이 사용됨
+				Console.WriteLine("Derived.AbstractMethodA()");
+			PrivateMethodA();
+			}
+		}
+		class Program
+		{
+			static void Main(string[] args)
+			{
+			AbstractBase obj = new Derived();
+			obj.AbstractMethodA();
+			obj.PublicMethodA();
+			}
+		}
+	}
