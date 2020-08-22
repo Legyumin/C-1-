8e878fb3a3f6a03f1c8c2cd5bd626b31bf010e7d
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-08-22 14:25:10 +0900

    무명형식/인터페이스/프로퍼티/추상클래스

---
+++ Ex9_1/Ex9_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex9_1
+{
+    class NameCard
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        {
+            get; set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+            MyCard.Age = 24;
+            MyCard.Name = "상현";
+            Console.WriteLine("나이 : {0}", MyCard.Age);
+            Console.WriteLine("이름 : {0}", MyCard.Name);
+        }
+    }
+}
---
+++ PropertiesInInterface/PropertiesInInterface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name
+        {
+            get;set;
+        }
+        string Value
+        {
+            get;set;
+        }
+    }
+
+    class NameValue : INamedValue
+    {
+        public string Name
+        {
+            get;set;
+        }
+        public string Value
+        {
+            get;set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameValue name = new NameValue() {Name="이름",Value="이규민" };
+            NameValue height = new NameValue() { Name = "키", Value = "180cm" };
+            NameValue weight = new NameValue() { Name = "몸무게", Value = "70kg" };
+
+            Console.WriteLine($"{name.Name} : {name.Value}");
+            Console.WriteLine($"{height.Name} : {height.Value}");
+            Console.WriteLine($"{weight.Name} : {weight.Value}");
+        }
+    }
+}
