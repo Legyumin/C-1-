398bafb4d8b5c0623ad3f2b945e8d43f6052d3a6
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-08-25 03:11:15 +0900

    Arrays

---
+++ 2DArray/2DArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1);j++)
+                {
+                    Console.WriteLine($"[{i},{j}] : { arr[i,j]} "); 
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ InintializingArray/InintializingArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InintializingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[3] { "안녕", "Hello", "Helo" };
+            foreach (string greeting in array1)
+            {
+                Console.WriteLine($" {greeting}");
+            }
+            Console.WriteLine();
+
+            string[] array2 = new string[] {"안녕", "Hello","Helo"};
+            foreach (string greeting in array2)
+            {
+                Console.WriteLine($" {greeting}");
+            }
+            Console.WriteLine();
+            string[] array3 = { "안녕", "Hello", "Helo" };
+            foreach (string greeting in array2)
+            {
+                Console.WriteLine($" {greeting}");
+            }
+        }
+    }
+}
