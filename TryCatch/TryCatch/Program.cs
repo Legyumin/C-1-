f60fd3bd2de29f9a2c0811128bb632a72262273d
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-09-02 02:28:50 +0900

    예외 처리

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"예외 발생 : {e.Message}");
+            }
+           
+           /* catch(DivideByZeroException e)
+            {
+                Console.WriteLine($"예외 발생2 : {e.Message}");
+            } */
+        }
+    }
+}
