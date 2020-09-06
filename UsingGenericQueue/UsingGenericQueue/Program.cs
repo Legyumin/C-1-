a00c759a7b387e7defc34133dcde728561bfe25e
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-09-06 18:14:07 +0900

    일반화 프로그래밍

---
+++ UsingGenericQueue/UsingGenericQueue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGenricQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                queue.Enqueue(i + 1);
+            }
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
---
+++ UsingGenricStack/UsingGenricStack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace usingGenricStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < 5; i++)
+                stack.Push(i + 1);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+        }
+    }
+}
