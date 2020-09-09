4037e97333a6c1bd7cc722031d033592ba4e8977
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-09-09 17:40:13 +0900

    Add files via upload

---
+++ usingApplication/usingApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace usingApplication
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+            form.Click += new EventHandler((sender,eventArgs) =>
+            {
+                Console.WriteLine("Closing Window");
+                Application.Exit();
+            }
+            );
+            Console.WriteLine("String...");
+            Application.Run(form);
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
