e7b7883e3dc63e26429d482136d8f09f4dda2763
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-09-11 16:04:45 +0900

    Add files via upload

---
+++ LoginApp/LoginApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\source\repos\LoginApp\LoginApp\LoignDB.mdf;Integrated Security=True");
+            string query = "SELECT * FROM Login WHERE Username='"+ textBox1.Text.Trim()+"'"+"and Password='"+ textBox1.Text.Trim()+"'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if(dtbl.Rows.Count == 1)
+            {
+                Form2 form2 = new Form2();
+                this.Hide();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("이름이나 비밀번호를 체크하세요.");
+            }
+        }
+    }
+}
